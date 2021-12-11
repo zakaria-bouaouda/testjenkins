0042d0c32b39387b14bcaa21e9d5797478f18e39
Reda El Ouahabi <redaelouahabi98@gmail.com>
2021-12-11 23:30:40 +0100

    Update test.java

--- test.java
+++ test.java
@@ -2,7 +2,8 @@ public class test {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        //test fails [email trigger]
+        System.out.prin("Hello, World");
     }
 
 }
